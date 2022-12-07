21530b9873b1072a46bae103017b3b66866ed1b5
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2022-12-06 21:11:56 -0300

    lacorepit
    laço de repetição

---
+++ apps.js
@@ -0,0 +1,16 @@
+//enquanto a condição for verdadeira faça algo
+let contador = 0
+while (contador<=10){
+    console.log (contador)
+    contador= contador + 1
+}
+// console.log("terminou")
+// function tabuada(){
+// let cont = 0
+// let n1 = document.getElementById("n1").value
+// while(cont<=10){
+//     console.log("n1" + cont + "=" + cont *n1)
+//     document.getElementById("resposta").innerHTML += (n1 + "x" + cont + "=" + cont *n1+ "<br>")
+//     cont++
+// }
+// }
